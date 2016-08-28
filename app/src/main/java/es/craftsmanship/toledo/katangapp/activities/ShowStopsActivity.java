4047c929060ed38718e2a6316b01339c6e3f4a58
Javier Gamarra <nhpatt@gmail.com>
2016-08-28 19:34:25 +0200

    import

--- app/src/main/java/es/craftsmanship/toledo/katangapp/activities/ShowStopsActivity.java
+++ app/src/main/java/es/craftsmanship/toledo/katangapp/activities/ShowStopsActivity.java
@@ -8,7 +8,6 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import android.widget.Toast;
